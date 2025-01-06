9618892cad1cc6a2a8672c7faed0302e536d93c2
dalikhouaja008 <mohamedali.khouaja@esprit.tn>
2025-01-06 20:38:48 +0100

    corriger partie timeout

--- app/src/main/java/com/example/b_rich/data/network/RetrofitClient.kt
+++ app/src/main/java/com/example/b_rich/data/network/RetrofitClient.kt
@@ -3,15 +3,25 @@ package com.example.b_rich.data.network
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
     private const val BASE_URL = "http://192.168.1.102:3000/"
+    private const val TIMEOUT_CONNECT = 60L // 30 secondes
+    private const val TIMEOUT_READ = 180L   // 2 minutes pour les longues opérations
+    private const val TIMEOUT_WRITE = 60L   // 30 secondes
     private val authInterceptor = AuthInterceptor()
 
 
     private val retrofit by lazy {
-        val client = OkHttpClient.Builder().addInterceptor(authInterceptor).build()
+        val client = OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+            .connectTimeout(TIMEOUT_CONNECT, TimeUnit.SECONDS)
+            .readTimeout(TIMEOUT_READ, TimeUnit.SECONDS)
+            .writeTimeout(TIMEOUT_WRITE, TimeUnit.SECONDS)
+            .retryOnConnectionFailure(true)
+            .build()
 
         Retrofit.Builder()
             .baseUrl(BASE_URL)
